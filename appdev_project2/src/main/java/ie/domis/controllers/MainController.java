3e6354f987a934f5a5bd6542ed76f841cec285d0
Dominik Wiecek <domis.wiecek@gmail.com>
2019-11-18 20:17:55 +0000

    Added bids to job page in controller

--- appdev_project2/src/main/java/ie/domis/controllers/MainController.java
+++ appdev_project2/src/main/java/ie/domis/controllers/MainController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import ie.domis.domain.Bid;
 import ie.domis.domain.Job;
+import ie.domis.service.BidService;
 import ie.domis.service.JobService;
 
 @Controller
@@ -17,6 +19,9 @@ public class MainController {
 	
 	@Autowired
 	JobService jobService;
+	
+	@Autowired
+	BidService bidService;
 	
 	@Secured({"ROLE_USER"})//"ROLE_ADMIN", 
 	@GetMapping(value= {"/", "/index"})
@@ -35,7 +40,9 @@ public class MainController {
 			model.addAttribute("id", id);
 			return "doesnotexist";
 		}
+		List<Bid> bids = bidService.findAllBidsByJob(id);
 		model.addAttribute("job", job);
+		model.addAttribute("bids", bids);
 		return "job";
 	}
 
