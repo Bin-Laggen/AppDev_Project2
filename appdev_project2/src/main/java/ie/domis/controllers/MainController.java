2a5031a5e3467f36b7a4e07736ab5d974ec5e9e4
Dominik Wiecek <domis.wiecek@gmail.com>
2019-11-18 19:23:57 +0000

    Added doesnotexist page in templates and job request in controller

--- appdev_project2/src/main/java/ie/domis/controllers/MainController.java
+++ appdev_project2/src/main/java/ie/domis/controllers/MainController.java
@@ -1,16 +1,37 @@
 package ie.domis.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import ie.domis.domain.Job;
+import ie.domis.service.JobService;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	JobService jobService;
 	
 	@Secured({"ROLE_USER"})//"ROLE_ADMIN", 
 	@GetMapping(value= {"/", "/index"})
 	public String handleIndexRequest() {
 		return "index";
+	}
+	
+	@Secured({"ROLE_USER"})//"ROLE_ADMIN", 
+	@GetMapping(value= {"/job/{id}"})
+	public String handleJobRequest(@PathVariable("id") int id, Model model) {
+		Job job = jobService.findJobById(id);
+		if (job != null) {
+			model.addAttribute("id", id);
+			return "doesnotexist";
+		}
+		model.addAttribute("job", job);
+		return "job";
 	}
 
 }
